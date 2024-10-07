57dc008ad23b64d70038f1a6990235df76db6c26
SamEBaker <esparzs1@my.erau.edu>
2024-10-07 12:28:01 -0700

    projFiles

---
+++ Assets/Script/Beggar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Beggar : INPC
+{
+    public void Speak()
+    {
+        Debug.Log("Do yoiu have some change to spare?");
+    }
+}
